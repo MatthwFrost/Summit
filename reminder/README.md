e2d793565580ef02bc87993c1dc88e1b60f39517
matthwfrost <matthwfrost@gmail.com>
2023-04-05 23:38:49 +0100

    Basic changes, cleaning some code up

---
+++ reminder/README.md
@@ -0,0 +1,10 @@
+# Reminder program
+Fetches data from google sheets and send to an api for future programs to use.
+
+
+### Basic commands
+
+gcloud app deploy
+gcloud app browse
+gcloud app logs tail -s default
+
--- reminder/main.py
+++ reminder/main.py
@@ -7,9 +7,8 @@ MAY SPLIT IT UP IN THE FUTURE.
 '''
 
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api, Resource
-import os
 
 # Own packages
 from sheet_data import sheet_data
@@ -22,10 +21,25 @@ app = Flask(__name__)
 api = Api(app)
 
 # The ID and range of reminder spreadsheet.
-SPREADSHEET_ID = os.environ.get('SPREADSHEET')
+SPREADSHEET_ID = "ADD THIS IN"
 RANGE_NAME = 'Sheet1!A:C'
 DATE_FORMAT = '%Y-%m-%d'
 today = datetime.today().date()
+
+
+@app.route("/")
+def hello_world():
+    return "<h1> Mirror Project </h1>\
+            <p>Home page for the mirror project</p>\
+            <p>API endpoints:</p>\
+            <br><p> <br>- /reminder <br># Get the revision reminders from the google sheet</p>", 200
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return "<h1>Oops, can't find what you are looking for</h1>\
+            <p>You have probably just found this project burried in a file somewhere and forgot all the links</p>", 404
+
 
 class Reminder(Resource):
     def get(self):
